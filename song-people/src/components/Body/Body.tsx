feda95809922d47fa175488fb3d9763fabf0d1d0
ShadowFox35 <anastasia97chernova@gmail.com>
2023-02-05 23:48:57 +0100

    feat: basic layout

--- song-people/src/components/Body/Body.tsx
+++ song-people/src/components/Body/Body.tsx
@@ -22,6 +22,7 @@ const Body: React.FC = () => {
         <div className="game">
           <div className="player">
             <img
+              className="player_img"
               src={`${process.env.PUBLIC_URL}/assets/song_images/Zefirka.svg`}
               alt=""
             />
@@ -30,20 +31,51 @@ const Body: React.FC = () => {
             </div>
           </div>
           <div className="options">
-            <div className="artists">Леди Гага</div>
-            <div className="info">
-              <img
-                className="info_img"
-                src={`${process.env.PUBLIC_URL}/assets/song_images/Zefirka.svg`}
-                alt=""
-              />
-              <p className="info_text">
-                инфо об исполнителе
-              </p>
+            <div className="options_wrapper">
+              {' '}
+              <div className="artists">
+                <ul className="artists_list">
+                  <li className="artists_list_item">
+                    Lorem, ipsum.
+                  </li>
+                  <li className="artists_list_item">
+                    Lorem, ipsum.
+                  </li>
+                  <li className="artists_list_item">
+                    Lorem, ipsum.
+                  </li>
+                  <li className="artists_list_item">
+                    Lorem, ipsum.
+                  </li>
+                  <li className="artists_list_item">
+                    Lorem, ipsum.
+                  </li>
+                </ul>
+              </div>
+              <div className="info">
+                <img
+                  className="info_img"
+                  src={`${process.env.PUBLIC_URL}/assets/song_images/Zefirka.svg`}
+                  alt=""
+                />
+                <p className="info_text">
+                  инфо об исполнителе Lorem ipsum dolor sit,
+                  amet consectetur adipisicing elit. Lorem
+                  ipsum dolor sit, amet consectetur
+                  adipisicing elit. Accusantium omnis animi
+                  delectus. Quibusdam, quidem ad? Tempore.
+                  Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Quis, nulla?
+                  Consequuntur tempora dolorem nemo
+                  accusamus! Lorem ipsum dolor sit amet
+                  consectetur adipisicing. Lorem ipsum dolor
+                  sit amet consectetur.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <button>Next Level</button>
+        <button className="button-next">Next Level</button>
       </div>
     </div>
   );
--- song-people/src/components/Footer/Footer.tsx
+++ song-people/src/components/Footer/Footer.tsx
@@ -6,27 +6,27 @@ const Footer: React.FC = () => {
   return (
     <div className="footer">
       <div className="wrapper">
-        <div className="github">
+        <div className='rights'>
           <p>©</p>
           <p>2023</p>
-          <p>
-            <a
-              className="github_link"
-              target="blank"
-              href="https://github.com/ShadowFox35">
-              github: ShadowFox35
-            </a>
-          </p>
-
-          <p>
-            <a
-              className="linkedIn_link"
-              target="blank"
-              href="https://linkedin.com/in/anastasia-chernova-dev">
-              LinkedIn: Anastasia Chernova
-            </a>
-          </p>
         </div>
+        <p className="github">
+          <a
+            className="github_link"
+            target="blank"
+            href="https://github.com/ShadowFox35">
+            github: ShadowFox35
+          </a>
+        </p>
+
+        <p className="linkedIn">
+          <a
+            className="linkedIn_link"
+            target="blank"
+            href="https://linkedin.com/in/anastasia-chernova-dev">
+            LinkedIn: Anastasia Chernova
+          </a>
+        </p>
       </div>
     </div>
   );
