d0fd4d7909404be7e30bc7705b6684d97613adb5
ShadowFox35 <anastasia97chernova@gmail.com>
2023-02-14 16:54:31 +0100

    refactor: types

--- song-people/src/components/Body/Body.tsx
+++ song-people/src/components/Body/Body.tsx
@@ -1,22 +1,69 @@
-import React, { useEffect, useRef } from 'react';
-import { navElemType } from '../../types/Objects';
-import { musicArray } from '../../constants/musicArray';
+import React, { useState, useEffect, useRef } from 'react';
+import { musicElemType } from '../../types/Objects';
+import {
+  genresArray,
+  allSongsArray,
+} from '../../constants/musicArray';
 // import { goodElemType } from '../../type/Objects';
 import './Body.scss';
 import './player.scss';
 import AudioPlayer from 'react-h5-audio-player';
 
 const Body: React.FC = () => {
+  const [level, setLevel] = useState<string>(
+    genresArray[0]
+  );
+  // const [popArray, rockArray] = allSongsArray;
+
+  const [song, setSong] = useState<musicElemType>(
+    allSongsArray[0][0]
+  );
+
+  const startGame = () => {
+    appointRandomSong();
+    chooseLevel();
+  };
+
+  const chooseLevel = () => {
+    console.log('chooseLevel');
+  };
+
+  // const gameArray: musicElemType[] = [];
+
+  const appointRandomSong = () => {
+    let randomSong = Math.floor(
+      Math.random() * allSongsArray[0].length
+    );
+    setSong(allSongsArray[0][randomSong]);
+    console.log('appointRandomSong', song);
+  };
+
+  const checkAnswer = (answer: string) => {
+    console.log('checkAnswer', answer);
+    if (answer === song.artist) {
+      answerRight();
+    } else {
+      answerWrong();
+    }
+  };
+
+  const answerWrong = () => {
+    console.log('answerWrong');
+  };
+
+  const answerRight = () => {
+    console.log('answerRight');
+  };
+
   return (
     <div className="body">
       <div className="body_wrapper">
         <ul className="genres-list">
-          {musicArray.map((song: navElemType) => (
-            <li className="genres-list_item">
-              {song.genre}
-            </li>
+          {genresArray.map((song: string) => (
+            <li className="genres-list_item">{song}</li>
           ))}
         </ul>
+        {/* {popArray.map((song: musicElemType) => ( */}
         <div className="game">
           <div className="player">
             <img
@@ -26,7 +73,7 @@ const Body: React.FC = () => {
             />
             <div className="player_wrapper">
               <AudioPlayer
-                src={`${process.env.PUBLIC_URL}/assets/music/BillieEilish.mp3`}
+                src={`${process.env.PUBLIC_URL}/assets/music/${song.audio}`}
                 customAdditionalControls={[]}
                 showJumpControls={false}
                 autoPlayAfterSrcChange={false}
@@ -38,47 +85,36 @@ const Body: React.FC = () => {
               {' '}
               <div className="artists">
                 <ul className="artists_list">
-                  <li className="artists_list_item">
-                    Lorem, ipsum.
-                  </li>
-                  <li className="artists_list_item">
-                    Lorem, ipsum.
-                  </li>
-                  <li className="artists_list_item">
-                    Lorem, ipsum.
-                  </li>
-                  <li className="artists_list_item">
-                    Lorem, ipsum.
-                  </li>
-                  <li className="artists_list_item">
-                    Lorem, ipsum.
-                  </li>
+                  {allSongsArray[0].map(
+                    (song: musicElemType) => (
+                      <li
+                        className="artists_list_item"
+                        onClick={() =>
+                          checkAnswer(song.artist)
+                        }>
+                        {song.artist}
+                      </li>
+                    )
+                  )}
                 </ul>
               </div>
               <div className="info">
                 <img
                   className="info_img"
-                  src={`${process.env.PUBLIC_URL}/assets/song_images/Zefirka.svg`}
+                  src={`${process.env.PUBLIC_URL}/assets/song_images/${song.img}`}
                   alt=""
                 />
-                <p className="info_text">
-                  инфо об исполнителе Lorem ipsum dolor sit,
-                  amet consectetur adipisicing elit. Lorem
-                  ipsum dolor sit, amet consectetur
-                  adipisicing elit. Accusantium omnis animi
-                  delectus. Quibusdam, quidem ad? Tempore.
-                  Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Quis, nulla?
-                  Consequuntur tempora dolorem nemo
-                  accusamus! Lorem ipsum dolor sit amet
-                  consectetur adipisicing. Lorem ipsum dolor
-                  sit amet consectetur.
-                </p>
+                <p className="info_text">{song.info}</p>
               </div>
             </div>
           </div>
         </div>
-        <button className="button-next">Next Level</button>
+        {/* ))} */}
+        <button
+          className="button-next"
+          onClick={() => startGame()}>
+          Next Level
+        </button>
       </div>
     </div>
   );
--- song-people/src/constants/musicArray.tsx
+++ song-people/src/constants/musicArray.tsx
@@ -1,34 +1,138 @@
-import { navElemType } from '../types/Objects';
 import { musicElemType } from '../types/Objects';
 
-export const musicArray: navElemType[] = [
-  {
-    genre: 'Pop',
-  },
-
-  {
-    genre: 'Rock',
-  },
-
-  {
-    genre: 'Metal',
-  },
-
-  {
-    genre: 'Rap',
-  },
-
-  {
-    genre: 'Electro',
-  },
-
-  {
-    genre: 'Classic',
-  },
+export const genresArray: string[] = [
+  'Pop',
+  'Rock',
+  'Metal',
+  'Rap',
+  'Electro',
+  'Classic',
 ];
 
-export const popArray: musicElemType[] = [
-  {
-    img: 'assets/song_images/Zefirka.svg',
-  },
+export const allSongsArray: musicElemType[][] = [
+  [
+    {
+      artist: 'Billie Eilish',
+      img: '/BillieEilish.jpg',
+      audio: 'pop/BillieEilish.mp3',
+      genre: 'Pop',
+      info: "Billie Eilish is an American singer-songwriter. She first gained public attention in 2015 with her debut single 'Ocean Eyes', written and produced by her brother Finneas O'Connell, with whom she collaborates on music and live shows. In 2017, she released her debut extended play (EP), titled Don't Smile at Me. Commercially successful, it reached the top 15 of record charts in numerous countries, including the US, UK, Canada, and Australia.",
+    },
+
+    {
+      artist: 'Britney Spears',
+      img: '/BritneySpears.jpg',
+      audio: 'pop/BritneySpears.mp3',
+      genre: 'Pop',
+      info: "Britney Jean Spears (born December 2, 1981) is an American singer. Often referred to as the 'Princess of Pop', she is credited with influencing the revival of teen pop during the late 1990s and early 2000s. Regarded as a pop icon, Spears has sold over 100 million records worldwide, including over 70 million in the United States, making her one of the world's best-selling music artists.",
+    },
+
+    {
+      artist: 'Haddway',
+      img: '/Haddway.jpg',
+      audio: 'pop/Haddway.mp3',
+      genre: 'Pop',
+      info: 'Nestor Alexander Haddaway (born 9 January 1965) is a Trinidadian-born German singer best known for his 1993 hit single "What Is Love", which reached number 1 in 13 countries. In 1992, Haddaway was signed by German label Coconut Records. His debut single, "What Is Love", rapidly became popular in Europe, reaching number 2 in Germany and the United Kingdom.',
+    },
+
+    {
+      artist: 'Lady Gaga',
+      img: '/LadyGaga.jpg',
+      audio: 'pop/LadyGaga.mp3',
+      genre: 'Pop',
+      info: 'Stefani Joanne Angelina Germanotta (born March 28, 1986), known as Lady Gaga, is an American singer, songwriter and actress. She is known for her image reinventions and musical versatility. Gaga was named the "Queen of Pop" in a 2011 ranking by Rolling Stone based on record sales and social media metrics. Gaga has won thirteen Grammy Awards, an Academy Award and two Golden Globe Awards.',
+    },
+
+    {
+      artist: 'One Republic',
+      img: '/OneRepublic.jpg',
+      audio: 'pop/OneRepublic.mp3',
+      genre: 'Pop',
+      info: 'One Republic is an American pop rock band formed in Colorado Springs, Colorado,[5] in 2002. OneRepublic have won several music awards and landed many nominations, including ones for American Music Awards, Billboard Music Awards, World Music Awards, and Grammy Awards. Separately, lead singer Ryan Tedder has won three Grammy Awards due to his songwriting credits for Adele and Taylor Swift.',
+    },
+  ],
+
+  [
+    {
+      artist: 'Green Day',
+      img: '/GreenDay.jpg',
+      audio: 'rock/GreenDay.mp3',
+      genre: 'Rock',
+      info: 'Green Day is an American rock band formed in the East Bay of California in 1987 by lead vocalist and guitarist Billie Joe Armstrong, together with bassist and backing vocalist Mike Dirnt. Green Day is credited with popularizing mainstream interest in punk rock in the United States, particularly with the album Dookie, which was cited by Fuse as the most important pop-punk album of all time.',
+    },
+
+    {
+      artist: 'Three Days Grace',
+      img: '/BritneySpears.jpg',
+      audio: 'rock/ThreeDaysGrace.mp3',
+      genre: 'Rock',
+      info: 'Three Days Grace is a Canadian rock band formed in Norwood, Ontario in 1992. The bands original line-up consisted of guitarist and lead vocalist Adam Gontier, drummer and backing vocalist Neil Sanderson, bassist Brad Walst. In 2003, Barry Stock was recruited as the bands lead guitarist, making them a quartet. In 2013, Gontier left the band and was replaced by Matt Walst, the younger brother of Brad Walst.',
+    },
+
+    {
+      artist: 'Haddway',
+      img: '/Haddway.jpg',
+      audio: 'rock/Haddway.mp3',
+      genre: 'Rock',
+      info: 'Nestor Alexander Haddaway (born 9 January 1965) is a Trinidadian-born German singer best known for his 1993 hit single "What Is Love", which reached number 1 in 13 countries. In 1992, Haddaway was signed by German label Coconut Records. His debut single, "What Is Love", rapidly became popular in Europe, reaching number 2 in Germany and the United Kingdom.',
+    },
+
+    {
+      artist: 'Lady Gaga',
+      img: '/LadyGaga.jpg',
+      audio: 'rock/LadyGaga.mp3',
+      genre: 'Rock',
+      info: 'Stefani Joanne Angelina Germanotta (born March 28, 1986), known as Lady Gaga, is an American singer, songwriter and actress. She is known for her image reinventions and musical versatility. Gaga was named the "Queen of Pop" in a 2011 ranking by Rolling Stone based on record sales and social media metrics. Gaga has won thirteen Grammy Awards, an Academy Award and two Golden Globe Awards.',
+    },
+
+    {
+      artist: 'One Republic',
+      img: '/OneRepublic.jpg',
+      audio: 'rock/OneRepublic.mp3',
+      genre: 'Rock',
+      info: 'OneRepublic is an American pop rock band formed in Colorado Springs, Colorado,[5] in 2002. OneRepublic have won several music awards and landed many nominations, including ones for American Music Awards, Billboard Music Awards, World Music Awards, and Grammy Awards. Separately, lead singer Ryan Tedder has won three Grammy Awards due to his songwriting credits for Adele and Taylor Swift.',
+    },
+  ],
 ];
+
+// export const rockArray: musicElemType[] = [
+//   {
+//     artist: 'Green Day',
+//     img: '/GreenDay.jpg',
+//     audio: 'rock/GreenDay.mp3',
+//     genre: 'Rock',
+//     info: 'Green Day is an American rock band formed in the East Bay of California in 1987 by lead vocalist and guitarist Billie Joe Armstrong, together with bassist and backing vocalist Mike Dirnt. Green Day is credited with popularizing mainstream interest in punk rock in the United States, particularly with the album Dookie, which was cited by Fuse as the most important pop-punk album of all time.',
+//   },
+
+//   {
+//     artist: 'Three Days Grace',
+//     img: '/BritneySpears.jpg',
+//     audio: 'rock/ThreeDaysGrace.mp3',
+//     genre: 'Rock',
+//     info: 'Three Days Grace is a Canadian rock band formed in Norwood, Ontario in 1992. The bands original line-up consisted of guitarist and lead vocalist Adam Gontier, drummer and backing vocalist Neil Sanderson, bassist Brad Walst. In 2003, Barry Stock was recruited as the bands lead guitarist, making them a quartet. In 2013, Gontier left the band and was replaced by Matt Walst, the younger brother of Brad Walst.',
+//   },
+
+//   {
+//     artist: 'Haddway',
+//     img: '/Haddway.jpg',
+//     audio: 'rock/Haddway.mp3',
+//     genre: 'Rock',
+//     info: 'Nestor Alexander Haddaway (born 9 January 1965) is a Trinidadian-born German singer best known for his 1993 hit single "What Is Love", which reached number 1 in 13 countries. In 1992, Haddaway was signed by German label Coconut Records. His debut single, "What Is Love", rapidly became popular in Europe, reaching number 2 in Germany and the United Kingdom.',
+//   },
+
+//   {
+//     artist: 'Lady Gaga',
+//     img: '/LadyGaga.jpg',
+//     audio: 'rock/LadyGaga.mp3',
+//     genre: 'Rock',
+//     info: 'Stefani Joanne Angelina Germanotta (born March 28, 1986), known as Lady Gaga, is an American singer, songwriter and actress. She is known for her image reinventions and musical versatility. Gaga was named the "Queen of Pop" in a 2011 ranking by Rolling Stone based on record sales and social media metrics. Gaga has won thirteen Grammy Awards, an Academy Award and two Golden Globe Awards.',
+//   },
+
+//   {
+//     artist: 'One Republic',
+//     img: '/OneRepublic.jpg',
+//     audio: 'rock/OneRepublic.mp3',
+//     genre: 'Rock',
+//     info: 'OneRepublic is an American pop rock band formed in Colorado Springs, Colorado,[5] in 2002. OneRepublic have won several music awards and landed many nominations, including ones for American Music Awards, Billboard Music Awards, World Music Awards, and Grammy Awards. Separately, lead singer Ryan Tedder has won three Grammy Awards due to his songwriting credits for Adele and Taylor Swift.',
+//   },
+// ];
